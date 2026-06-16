3f059688e2112f80638e9dfb502f02e6c9f48486
anubhutisinha04 <asinha1@bnl.gov>
2026-06-16 14:13:37 -0400

    run the container as a non root user

--- frontend/src/app/pages/TiledDataPage.tsx
+++ frontend/src/app/pages/TiledDataPage.tsx
@@ -3,7 +3,7 @@ export default function TiledDataPage() {
 
     return (
         <div className="flex flex-col items-center justify-start gap-4 py-4 max-w-96 mx-auto">
-            <Tiled apiKey='secret' size='large'/>
+            <Tiled size='large'/>
         </div>
     );
 }
